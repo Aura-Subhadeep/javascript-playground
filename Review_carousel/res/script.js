d1366947727b7a2e0c5d1a94e8210fb7d2d2c804
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-05-07 21:36:37 +0530

    Update script.js

---
+++ Review_carousel/res/script.js
@@ -0,0 +1,59 @@
+// review data
+const reviews = [
+    {
+        id: 1,
+        name: "John Smith",
+        profession: "Web Developer",
+        review: "I was amazed by the quality of the service. The team was professional and delivered outstanding results. Highly recommended!",
+        image: "/Review_carousel/assets/img/pexels-anastasia-shuraeva-6608313.jpg"
+    },
+    {
+        id: 2,
+        name: "Emily Johnson",
+        profession: "Graphic Designer",
+        review: "Working with this team was a fantastic experience. They understood my requirements and brought my vision to life. I'm extremely satisfied!",
+        image: "/Review_carousel/assets/img/pexels-andrea-piacquadio-774909.jpg"
+    },
+    {
+        id: 3,
+        name: "David Thompson",
+        profession: "Marketing Manager",
+        review: "The marketing strategies implemented by this company have been a game-changer for our business. Our sales have increased significantly. Great job!",
+        image: "/Review_carousel/assets/img/pexels-carol-wd-3454298.jpg"
+    },
+    {
+        id: 4,
+        name: "Sophia Davis",
+        profession: "Business Owner",
+        review: "I'm incredibly happy with the website they created for my business. It's visually stunning and user-friendly. The team was a pleasure to work with.",
+        image: "/Review_carousel/assets/img/pexels-cottonbro-studio-8090137.jpg"
+    },
+    {
+        id: 5,
+        name: "Daniel Wilson",
+        profession: "UX Designer",
+        review: "The UX design provided by this team exceeded my expectations. They truly understand user behavior and crafted an intuitive experience. Highly recommended!",
+        image: "/Review_carousel/assets/img/pexels-ketut-subiyanto-4307869.jpg"
+    },
+    {
+        id: 6,
+        name: "Olivia Brown",
+        profession: "Content Writer",
+        review: "The content they produced was of exceptional quality. Engaging, informative, and tailored to our target audience. I'm impressed with their writing skills.",
+        image: "/Review_carousel/assets/img/pexels-maria-orlova-4946515.jpg"
+    },
+    {
+        id: 7,
+        name: "Matthew Taylor",
+        profession: "E-commerce Manager",
+        review: "This team helped us optimize our e-commerce platform and streamline our processes. Our conversion rates have improved, and we've seen a boost in sales.",
+        image: "/Review_carousel/assets/img/pexels-pixabay-220453.jpg"
+    },
+    {
+        id: 8,
+        name: "Ava Martinez",
+        profession: "Social Media Influencer",
+        review: "I collaborated with this agency on a social media campaign, and the results were phenomenal. They know how to create engaging content that drives engagement.",
+        image: "/Review_carousel/assets/img/pexels-pixabay-415829.jpg"
+    }
+];
